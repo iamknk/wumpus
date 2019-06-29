789425c532d0451f179b179ea97664368bacadba
Kashif Nazir Khan <pb193105@gmail.com>
2019-06-29 13:17:50 +0500

    gold implemented

--- task.py
+++ task.py
@@ -1,25 +1,29 @@
-#empty = 0 pits = 1 wind = 2 wumpus = 3 smell = 4
+#empty = 0 pits = 1 wind = 2 wumpus = 3 smell = 4 gold = 5
 import random
 def windorsmell(matrix, x, y, size, windorsmell):
 	if(windorsmell == 2):
-		if(x-1 < (size ) and matrix[x-1][y] != (1 or 3)):
+		if(0 <= x-1 < (size ) and matrix[x-1][y] != (1 or 3)):
 			matrix[x-1][y] = windorsmell
-		if(x+1 < (size) and matrix[x+1][y] != (1 or 3)):
+		if(0 <= x+1 < (size) and matrix[x+1][y] != (1 or 3)):
 			matrix[x+1][y] = windorsmell
-		if(y-1 < (size) and matrix[x][y-1] != (1 or 3)):
+		if(0 <= y-1 < (size) and matrix[x][y-1] != (1 or 3)):
 			matrix[x][y-1] = windorsmell
-		if(y+1 < (size) and matrix[x][y+1] != (1 or 3)):
+		if(0 <= y+1 < (size) and matrix[x][y+1] != (1 or 3)):
 			matrix[x][y+1] = windorsmell
 	if(windorsmell == 4):
-		if(x-1 < (size ) and matrix[x-1][y] != (1 or 3)):
+		if(0 <= x-1 < (size ) and matrix[x-1][y] != (1 or 3)):
 			matrix[x-1][y] = int(str(matrix[x-1][y]) + "4")
-		if(x+1 < (size) and matrix[x+1][y] != (1 or 3)):
+		if(0 <= x+1 < (size) and matrix[x+1][y] != (1 or 3)):
 			matrix[x+1][y] = int(str(matrix[x+1][y]) + "4")
-		if(y-1 < (size) and matrix[x][y-1] != (1 or 3)):
+		if(0 <= y-1 < (size) and matrix[x][y-1] != (1 or 3)):
 			matrix[x][y-1] = int(str(matrix[x][y-1]) + "4")
-		if(y+1 < (size) and matrix[x][y+1] != (1 or 3)):
+		if(0 <= y+1 < (size) and matrix[x][y+1] != (1 or 3)):
 			matrix[x][y+1] = int(str(matrix[x][y+1]) +  "4")
-
+			
+def matrixtostring(matrix, size):
+	for i in range(size):
+		for j in range(size):
+			matrix[i][j] = str(matrix[i][j])
 
 
 def makematrix(size, pits):
@@ -31,9 +35,11 @@ def makematrix(size, pits):
 		
 		windorsmell(matrix, x, y, size, 2)
 	wumpus_flag = True
-	x = random.randint(0, size-1)
-	y = random.randint(0, size-1)
+	
 	while(wumpus_flag):
+		x = random.randint(0, size-1)
+		y = random.randint(0, size-1)
+		print(type(matrix[x][y]))
 		if(matrix[x][y] != 0):
 			if(matrix[x][y] == 2):
 				matrix[x][y] = str(matrix[x][y]) + "3"
@@ -45,10 +51,19 @@ def makematrix(size, pits):
 			matrix[x][y] = 3
 			wumpus_flag = False
 			windorsmell(matrix, x, y, size, 4)
+		gold_flag = True
+		while(gold_flag):
+			x = random.randint(0, size-1)
+			y = random.randint(0, size-1)
+			if(isinstance(matrix[x][y], int)):
+				if(matrix[x][y] == 0):
+					matrix[x][y] = 5
+					gold_flag = False
+
 
 	return matrix
 
-def printmatrix(matrix, size):
+def printmatrix(matrix, size): 
 	for i in range(size):
 		for j in range(size):
 			print(str(i) + "X" + str(j))
