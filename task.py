78720c160a55f85da1ccd1883a5a260674b3627d
Kashif Nazir Khan <pb193105@gmail.com>
2019-07-03 11:40:15 +0500

    Game Completed

--- task.py
+++ task.py
@@ -114,89 +114,184 @@ import random
 # 		print("You Won the Game")
 # 	else:
 # 		print("You Lost the Game")
+
+def printmatrix(matrix, size): 
+	for i in range(size):
+		for j in range(size):
+			print(str(matrix[i][j]), end=" ")
+		print()
+
 def matrixtostring(matrix, size):
 	for i in range(size):
 		for j in range(size):
 			matrix[i][j] = str(matrix[i][j])
 	return matrix
-def initializematrix(size):
-	return [[-1 for x in range(size)] for y in range(size)] 
-def game(matrix, size):
-	quest_matrix = matrixtostring(initializematrix(size))
-	print(quest_matrix)
+def initializematrix(size, number):
+	return [[number for x in range(size)] for y in range(size)] 
+def initialPosition(size):
 	current_point_flag = True
 	while(current_point_flag):
 		x = random.randint(0, size-1)
 		y = random.randint(0, size-1)
-		if(x == 0 or y == 0 or x == (size -1) or y == (size - 1) and ("1" in matrix[x][y] and "3" in matrix[x][y])):
+		print(str(x) + str(y))
+		if(x == 0 or y == 0 or x == (size -1) or y == (size - 1)):
 			current_point_flag = False
 
-	current_point = [x,y]
+	return x, y
+def game(matrix, size):
+	quest_matrix = matrixtostring(initializematrix(size, -1), size)
+	#x, y = initialPosition(size)
+	x = 0
+	y = 0
+	history = []
 	game_over_flag = False
 	game_result_flag = False
-	if("5" in matrix[x][y]):
-		game_over_flag = True
-		game_result_flag = True
+	
 	while(not game_over_flag):
+		move_flag = False
+		print(str(x) + " " + str(y))
+		print()
 		quest_matrix[x][y] = matrix[x][y]
+		
+		if("5" in matrix[x][y]):
+			game_over_flag = True
+			game_result_flag = True
+			print("You Won the game")
+		elif("3" in matrix[x][y] or "1" in matrix[x][y]):
+			game_over_flag = True
+			game_result_flag = False
+			print("You Lost the game")
+		flagone, flagTwo, flagThree, flagFour = True, True, True, True
+		directions = []
 		if("0"in quest_matrix[x][y]):
-			flagone, flagTwo, flagThree, flagFour = True, True, True, True
-			if(0 <= x-1 < (size)):
+
+			if(0 <= x-1 < (size) and "-1" in quest_matrix[x-1][y]):
 				quest_matrix[x-1][y] = "7"
-			elif(0 <= x+1 < (size)):
+				
+				
+			if(0 <= x+1 < (size) and "-1" in quest_matrix[x+1][y]):
 				quest_matrix[x+1][y] = "7"
-			elif(0 <= y-1 < (size)):
+				
+				
+			if(0 <= y-1 < (size) and "-1" in quest_matrix[x][y-1]):
 				quest_matrix[x][y-1] = "7"
-			elif(0 <= y+1 < (size)):
+				
+				
+			if(0 <= y+1 < (size) and "-1" in quest_matrix[x][y+1]):
 				quest_matrix[x][y+1] = "7"
+			#directions = [1,2,3,4]
+
+				
 		elif("2" in quest_matrix[x][y] or "4" in quest_matrix[x][y]):
-			flagone, flagTwo, flagThree, flagFour = False, False, False, False
-		if(0 <= x-1 < (size)):
-			if(flagone and not move_flag):
-				x = x-1
-				move_flag = True
-			else:
-				if("-1" in quest_matrix[x-1][y]):
-					quest_matrix[x-1][y] = "6"
+			if(0 <= x-1 < (size) and "-1" in quest_matrix[x-1][y]):
+				quest_matrix[x-1][y] = "6"
+				
+				
+			if(0 <= x+1 < (size) and "-1" in quest_matrix[x+1][y]):
+				quest_matrix[x+1][y] = "6"
+				
+			
+				
+			if(0 <= y-1 < (size) and "-1" in quest_matrix[x][y-1]):
+				quest_matrix[x][y-1] = "6"
 
-		if(0 <= x+1 < (size)):
-			if(flagTwo and not move_flag):
-				x = x+1
-				move_flag = True
-			else:
-				if("-1" in quest_matrix[x-1][y]):
-					quest_matrix[x+1][y] = "6"
-		if(0 <= y-1 < (size)):
-			if(flagThree and not move_flag):
-				y = y-1
-				move_flag = True
-			else:
-				if("-1" in quest_matrix[x-1][y]):
-					quest_matrix[x][y-1] = "6"
-		if(0 <= y+1 < (size)):
-			if(flagFour and not move_flag):
-				y = y + 1
-				move_flag = True
-			else:
-				if("-1" in quest_matrix[x-1][y]):
-					quest_matrix[x][y+1] = "6"
-		if(0 <= x-1 < (size) and not move_flag):
-			if("0" in quest_matrix[x-1][y] or "5" in quest_matrix[x-1][y]):
+			if(0 <= y+1 < (size) and "-1" in quest_matrix[x][y+1]):
+				quest_matrix[x][y+1] = "6"
+		if(0 <= x-1 < (size) and not("6" in quest_matrix[x-1][y])):
+			directions.append(1)
+		if(0 <= x+1 < (size) and not("6" in quest_matrix[x+1][y])):
+			directions.append(2)
+		if(0 <= y-1 < (size) and not("6" in quest_matrix[x][y-1])):
+			directions.append(3)
+		if(0 <= y+1 < (size) and not("6" in quest_matrix[x][y+1])):
+			directions.append(4)
+		doubt_probility = random.uniform(0.0, 1.0)
+		if(doubt_probility <= 0.2):
+			directions.extend([1,2,3,4])
 
-		elif(0 <= x+1 < (size)):
-			if(flagTwo):
-				x = x+1
-				move_flag = True
-		elif(0 <= y-1 < (size)):
-			if(flagThree):
-				y = y-1
-				#current_point = [x, y-1]
-				move_flag = True
-		elif(0 <= y+1 < (size)):
-			if(flagFour):
-				y = y + 1
-				#current_point = [x, y+1]
-				move_flag = True
+		printmatrix(quest_matrix, size)
+		if(len(directions) > 0):
+			print(directions)
+			move = random.choice(directions)
+			if(move == 1):
+				if(0 <= x-1 < (size)):
+					x = x - 1
+					print("Moving Top")
+					move_flag = True
+					history.append([x,y])
+			if(move == 2):
+				if(0 <= x+1 < (size)):
+					x = x +1
+					print("Moving Down")
+					move_flag = True
+					history.append([x,y])
+			if(move == 3):
+				if(0 <= y-1 < (size)):
+					y = y-1
+					print("Moving Left")
+					move_flag = True
+					history.append([x,y])
+			if(move == 4):
+				if(0 <= y+1 < (size)):
+					y = y+1	
+					print("Moving Right")
+					move_flag = True
+					history.append([x,y])
+
+
+				
+		
+		while(not move_flag):
+				print("All Sides have either smell or wind")
+				print("Where to move")
+				print("1. Move Top")
+				print("2. Move Down")
+				print("3. Move Left")
+				print("4. Move Right")
+				instruction = int(input(""))
+				if(instruction == 1):
+					if(0 <= x-1 < (size)):
+						x = x-1
+						print("Moving Top")
+						move_flag = True
+						history.append([x,y])
+					else:
+						print("Cannot move in this direction. Out of Area")
+				elif(instruction == 2):
+					if(0 <= x+1 < (size)):
+						x = x +1
+						print("Moving Down")
+						move_flag = True
+						history.append([x,y])
+					else:
+						print("Cannot move in this direction. Out of Area")
+				elif(instruction == 3):
+					if(0 <= y-1 < (size)):
+						y = y-1
+						print("Moving Left")
+						move_flag = True
+						history.append([x,y])
+					else:
+						print("Cannot move in this direction. Out of Area")
+				elif(instruction == 4):
+					if(0 <= y+1 < (size)):
+						y = y + 1
+						print("Moving Right")
+						move_flag = True
+						history.append([x,y])
+					else:
+						print("Cannot move in this direction. Out of Area")
+
+		if("5" in matrix[x][y]):
+			game_over_flag = True
+			game_result_flag = True
+			print("You Won the game")
+		elif("3" in matrix[x][y] or "1" in matrix[x][y]):
+			game_over_flag = True
+			game_result_flag = False
+			print("You Lost the game")
+				
+		
 
 		
 		
@@ -229,7 +324,7 @@ def windorsmell(matrix, x, y, size, windorsmell):
 
 
 def makematrix(size, pits):
-	matrix = initializematrix(size)
+	matrix = initializematrix(size, 0)
 	for i in range(pits):
 		x = random.randint(0, size-1)
 		y = random.randint(0, size-1)
@@ -264,27 +359,24 @@ def makematrix(size, pits):
 
 	return matrix
 
-def printmatrix(matrix, size): 
-	for i in range(size):
-		for j in range(size):
-			print(str(i) + "X" + str(j) + " = " + str(matrix[i][j]))
-			print()
+
  
 
 def main():
-	wumpus_size = int(input("Enter the size of wumpus "))
+	grid_size = int(input("Enter the size of wumpus "))
 	while(True):
 		pits_num = int(input("Enter number of pits "))
-		if(pits_num > wumpus_size):
+		if(pits_num > grid_size):
 			print("Pit Numbers Cannot be larger than wumpus")
 		else:
 			break
 	wumpus = 1
-	matrix = makematrix(wumpus_size, pits_num)
-	printmatrix(matrix, wumpus_size)
-	matrix = matrixtostring(matrix, wumpus_size)
+	matrix = makematrix(grid_size, pits_num)
+	printmatrix(matrix, grid_size)
+	matrix = matrixtostring(matrix, grid_size)
 	
-	game(matrix, wumpus_size)
+	game(matrix, grid_size)
+	#print(initialPosition(grid_size))
 
 	
 main()
